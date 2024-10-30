89334bba7edb9044d920b03de2e0d1b37073966c
Ben Johnson <jesu4332@ox.ac.uk>
2024-10-30 10:29:32 +0000

    I am committed

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,27 +1,48 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment (1).csv")
 
 logistic_fun <- function(t) {
   
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
   
-  return(N)
-  
-}
+  return(N)}
 
-N0 <- ??? #
-  
-r <- ??? #
-  
-K <- ??? #
 
-ggplot(aes(???,???), data = growth_data) +
+N0 <- exp(7.037e+00)
+ #initial pop 
+exp(7.037e+00)
+r <- 9.757e-03
   
-  geom_function(fun=logistic_fun, colour="red") +
+K <- 5.996e+10
+#carrying capacity
+ggplot(aes(t,N), data = growth_data) +
+  
+  geom_function(fun = logistic_fun, colour="red") +
   
   geom_point()
 
-  #scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10')
 
+#model2
+  logistic_fun <- function(t) {
+    
+    N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
+    
+    return(N)}
+  
+  
+  N0 <- 59188937427
+  
+  r <- 0
+  
+  K <- 5.996e+10
+  
+  ggplot(aes(t,N), data = growth_data) +
+    
+    geom_function(fun = logistic_fun, colour="red") +
+    
+    geom_point()
+  
+  scale_y_continuous(trans='log10')
 
